b866853ed8a2faba24a450c8c394e5f5907c6c13
xdjavit0 <99995052+xdjavit0@users.noreply.github.com>
2022-09-09 09:11:54 +0200

    Add files via upload

---
+++ calculator.steps.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-undef */
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { expect } = require('@playwright/test');
+
+const url = 'http://127.0.0.1:5500/calculator.html';
+// const url = 'https://mariaxyz14.github.io/Calculator/';
+// const url = 'https://eduardovazquezitb.github.io/Calculator/'; // Eduardo Vázquez
+// const url = 'https://raquellecu.github.io/calculator/'; // Raquel Lecuona
+// const url='https://walid243.github.io/Calculator_App/src/main/html/calculator.html'; // Walid
+
+async function buttonClick(buttonId) {
+	await page.click(`[data-testid="${buttonId}"]`, { force: true });
+}
+
+async function serialForEach(array, cb) {
+	return array.reduce((promise, data) => promise.then(() => cb(data)), Promise.resolve());
+}
+
+async function writeNumber(strNumber) {
+	let isNegative = false;
+	const digits = strNumber.split('');
+	await serialForEach(digits, async (digit) => {
+		if (digit === '-') {
+			isNegative = true;
+		} else {
+			await buttonClick(digit);
+		}
+	});
+	if (isNegative) {
+		await buttonClick('+-');
+	}
+}
+
+async function keyPress(keyId) {
+	await page.keyboard.press(keyId, { force: true });
+}
+
+Given('a user opens the app', async () => {
+	await page.goto(url);
+});
+
+Then('the display should show the following value: {string}', async (string) => {
+	const display = await page.locator('data-testid=display').innerText();
+	// const display = await page.locator('data-testid=display').inputValue();
+	expect(display).toBe(string);
+});
+
+Given('the display shows the following value: {string}', async (string) => {
+	await buttonClick('C');
+	await writeNumber(string);
+});
+
+When('the user presses the {string} button', async (string) => {
+	await buttonClick(string);
+});
+
+When('the user presses the {string} key', async (string) => {
+	await keyPress(string);
+});
+
+When('the user writes the number: {string}', async (string) => {
+	await writeNumber(string);
+});
+
+Then('the {string} button should be disabled', async (string) => {
+	const locator = page.locator(`[data-testid="${string}"]`);
+	await expect(locator).toBeDisabled();
+});
+
+Then('the {string} button should be enabled', async (string) => {
+	const locator = page.locator(`[data-testid="${string}"]`);
+	await expect(locator).toBeEnabled();
+});
